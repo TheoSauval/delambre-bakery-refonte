921154d9b292276a6ba703c540d100e7ed2ec7b1
Theo Code <115007685+TheoSauval@users.noreply.github.com>
2025-01-27 10:26:21 +0100

    refonte fini

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -1,6 +1,8 @@
 const Footer = () => (
-    <footer>
-        <p>© 2017 Company, Inc.</p>
+    <footer className="bg-foreground flex justify-between text-center text-white p-4">
+        <button className="hover:text-rougecerise transition ease-in-out">LUCKY TICKET</button>
+        <p>© 2025 Sauval Theo.</p>
+        <button className="hover:text-rougecerise transition ease-in-out">Mentions légales</button>
     </footer>
 );
 export default Footer;
